f41605326039995f053fd4bde018e3becb4ff1bc
Dmitrii Molochnikov <dmitrii_molochnikov@epam.com>
2018-04-06 21:52:24 +0300

    db-project was added

---
+++ BookStoreDb/dbo/Tables/CourseStudent.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CourseStudent] (
+    [CourseId]  INT NOT NULL,
+    [StudentId] INT NOT NULL,
+    CONSTRAINT [PK_dbo.CourseStudent] PRIMARY KEY CLUSTERED ([CourseId] ASC, [StudentId] ASC),
+    CONSTRAINT [FK_dbo.CourseStudent_dbo.Courses_CourseId] FOREIGN KEY ([CourseId]) REFERENCES [dbo].[Courses] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_dbo.CourseStudent_dbo.Students_StudentId] FOREIGN KEY ([StudentId]) REFERENCES [dbo].[Students] ([Id]) ON DELETE CASCADE
+);
+
