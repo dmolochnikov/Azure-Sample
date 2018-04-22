ac0a927bb6c147c5bf7893eb330b4b150fc2b884
Dmitrii Molochnikov <dmitrii_molochnikov@epam.com>
2018-04-22 20:02:06 +0300

    add AI, update packages

--- BookStore/App_Start/FilterConfig.cs
+++ BookStore/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace BookStore
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
             // filters.Add(new ErrorHandlerAttribute());
         }
     }
